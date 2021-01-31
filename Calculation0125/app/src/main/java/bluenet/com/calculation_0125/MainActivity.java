91765019cbe3da5d07c688e6bf69a480c4337c5c
Ryan <qazxvi15639@yahoo.com.tw>
2021-02-01 01:48:33 +0800

    fix bug

--- Calculation0125/app/src/main/java/bluenet/com/calculation_0125/MainActivity.java
+++ Calculation0125/app/src/main/java/bluenet/com/calculation_0125/MainActivity.java
@@ -3,12 +3,10 @@ package bluenet.com.calculation_0125;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.snackbar.BaseTransientBottomBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnDot; //按鈕0~9 + 小數點
     private TextView tvNumber;
     private String s1, s2, temp;
-    private Integer number1, number2, total,c=-1;
+    private Integer number1, number2, total, c = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,13 +66,34 @@ public class MainActivity extends AppCompatActivity {
 
     //數字0~9
     private View.OnClickListener myListner = new View.OnClickListener() {
-
         @Override
         public void onClick(View v) {
-
             temp = tvNumber.getText().toString();
+            Button btn = (Button) findViewById(v.getId());
+
             switch (v.getId()) {
+                // 判斷小數點前需有數字,尚未判斷整句是否只有一個小數點符號
+                case R.id.btn_dot:
+                    if ("".equals(temp)) {
+                    } else {
+                        tvNumber.setText(temp + btn.getText());
+                    }
+                    break;
+
+                //判斷啟始値是否為0,如為0只顯示數字0
                 case R.id.btn_zero:
+                    if ("".equals(temp)||String.valueOf(total)==temp) {
+                        tvNumber.setText(btn.getText());
+                    } else {
+                        if ("0".equals(temp) || ".".equals(temp)) {
+                            tvNumber.setText("0");
+                        } else {
+                            tvNumber.setText(temp + btn.getText());
+                        }
+                    }
+                    break;
+
+                //數字1-9顯示
                 case R.id.btn_one:
                 case R.id.btn_two:
                 case R.id.btn_three:
@@ -84,95 +103,93 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.btn_seven:
                 case R.id.btn_eight:
                 case R.id.btn_nine:
-                case R.id.btn_dot: // '.'
-                    Button btn = (Button) findViewById(v.getId());
-                    tvNumber.setText(temp + btn.getText());
-                    break;
-
-                case R.id.btn_ac:
-                    if (temp.length() == 1) {
-                        tvNumber.setText("");
+                    if ("0".equals(temp)||String.valueOf(total)==temp) {
+                        tvNumber.setText(btn.getText());
                     } else {
-                        tvNumber.setText(temp.substring(0, temp.length() - 1));
+                        tvNumber.setText(temp + btn.getText());
                     }
                     break;
 
+                //清除按鈕
+                case R.id.btn_ac:
+                    s1 = null;
+                    temp = "";
+                    tvNumber.setText(temp);
+                    break;
+
+                //等於符號
                 case R.id.btn_equal:
-                    if(s1 != null && s2 == null) {
-                        Count(c);
-                        c=9;
-                        Count(c);
-                        break;
-                    }
+                    if ("".equals(temp) || s1==null) {
 
+                    } else {
+                        Count(c);
+                        tvNumber.setText(String.valueOf(s1));
+                        c = -1;
+                        temp="";
+                    }
+                    break;
+
+                //加符號
                 case R.id.btn_add:
-                    if (s1 == null && s2 == null) {
-                        s1 = temp;
-                        tvNumber.setText("");
-                        c=0;
+                    if ("".equals(temp)) { //啟始無輸入值,不做動作
                         break;
-                    }
-                    if(s1 != null && s2 == null) {
-                       Count(c);
-                       tvNumber.setText(s1);
-                       break;
-                    }
-
-
-                case R.id.btn_subtraction:
-                    if (s1 == null && s2 == null) {
-                        s1 = temp;
-                        tvNumber.setText("");
-                        c=1;
+                    } else if (c == -1) { //判斷是否為第一次運算符號
+                        if (s1 == null) { //判斷是否為第一次數入數字
+                            s1 = temp;
+                            temp = "";
+                            tvNumber.setText(temp);
+                            c = 0;
+                        } else {
+                            Count(c);
+                            c = 0;
+                        }
                         break;
-                    }
-                    if(s1 != null && s2 == null) {
+                    } else {
                         Count(c);
-                        c=1;
-                        break;
+                        c = 0;
                     }
-            }
-        }
+                    break;
 
-        public void Count(int c) {
-            switch (c) {
-                case 0:
-                    s2 = temp;
-                    tvNumber.setText("");
-                    number1 = Integer.parseInt(s1);
-                    number2 = Integer.parseInt(s2);
-                    total = number1 + number2;
-                    s1 = String.valueOf(total);
-                    s2 = null;
-                    break;
-                case 1:
-                    s2 = temp;
-                    tvNumber.setText("");
-                    number1 = Integer.parseInt(s1);
-                    number2 = Integer.parseInt(s2);
-                    total = number1 - number2;
-                    s1 = String.valueOf(total);
-                    s2 = null;
-                    break;
-                case 2:
-                case 9:
-                    tvNumber.setText(String.valueOf(total));
+                    //減符號
+                case R.id.btn_subtraction:
+                    if ("".equals(temp)) { //啟始無輸入值,不做動作
+                        break;
+                    } else if (c == -1) { //判斷是否為第一次運算符號
+                        if (s1 == null) { //判斷是否為第一次數入數字
+                            s1 = temp;
+                            temp = "";
+                            tvNumber.setText(temp);
+                        } else {
+                            Count(c);
+                            c = 1;
+                        }
+                        break;
+                    } else {
+                        Count(c);
+                        c = 1;
+                    }
                     break;
             }
         }
     };
+
+
+    //判斷運算符
+    public void Count(int c) {
+        number1 = Integer.parseInt(s1);
+        number2 = Integer.parseInt(temp);
+        switch (c) {
+            case 0:
+                total = number1 + number2;
+                break;
+            case 1:
+                total = number1 - number2;
+                break;
+            case 2:
+                break;
+        }
+        s1 = String.valueOf(total);
+        temp = "";
+        tvNumber.setText(s1);
+    }
 }
-//                case R.id.btn_equal: // '='
-//                    for (int i = 0; i < tvNumber.length(); i++) {
-//                        if (s1.charAt(i) == '+' || s1.charAt(i) == '-' ||
-//                                s1.charAt(i) == 'x' || s1.charAt(i) == '÷') {
-//                            for (int j = i - 1; j >= 0; j--) {   //儲存計算符號"前"數字
-//                                A += s1.charAt(j);
-//                                Log.e("數字1", A);  /*Log*/
-//                            }
-//                            for (int k = i + 1; k < s1.length(); k++) { //儲存計算符號"後"數字
-//                                B += s1.charAt(k);
-//                                Log.e("數字2", B); /*Log*/
-//                            }
-//                        }
-//                    }
